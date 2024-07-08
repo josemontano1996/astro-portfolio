d299c425bd31e757d8c60b73fed92cb5d05525bf
Josema <jm3develop@gmail.com>
2024-07-08 15:42:23 +0200

    finished view transitions and responsive design

--- src/components/NavBar/LanguageSelector.tsx
+++ src/components/NavBar/LanguageSelector.tsx
@@ -1,21 +1,48 @@
+import React, { useRef } from 'react';
 import { supportedLocales } from '@/consts/locales';
 import { cn } from '../../lib/utils';
 
-export const LanguageSelector = ({ locale, styles }: { locale: string; styles?: string }) => {
+export const LanguageSelector = ({
+  locale,
+  styles,
+}: {
+  locale: string;
+  styles?: string;
+}) => {
+  // Create a ref to store the anchor tags
+  const anchorRefs = useRef<{ [key: string]: HTMLAnchorElement | null }>({});
+
   const navigateTo = (value: string) => {
-    window.location.href = `/${value}`;
+    // Trigger a click on the respective anchor tag
+    if (anchorRefs.current[value]) {
+      anchorRefs.current[value]?.click();
+    }
   };
+
   return (
-    <select
-      className={cn('bg-background', styles)}
-      onChange={(e) => navigateTo(e.target.value)}
-      defaultValue={locale}
-    >
-      {supportedLocales.map((loc) => (
-        <option key={loc} value={loc} className='p-1'>
-          {loc.toUpperCase()}
-        </option>
-      ))}
-    </select>
+    <>
+      <select
+        className={cn('bg-background', styles)}
+        onChange={(e) => navigateTo(e.target.value)}
+        defaultValue={locale}
+      >
+        {supportedLocales.map((loc) => (
+          <option key={loc} value={loc}>
+            {loc.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <div className="hidden">
+        {supportedLocales.map((loc) => (
+          <a
+            key={loc}
+            href={`/${loc}`}
+            ref={(el) => (anchorRefs.current[loc] = el)}
+          >
+            {loc.toUpperCase()}
+          </a>
+        ))}
+      </div>
+    </>
   );
 };
